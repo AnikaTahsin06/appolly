43e45fa1f3d7138290198cd4bc2d5c19280a80c0
anikatahsin12 <bushratahsin653@gmail.com>
2022-08-27 23:53:27 +0600

    another

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,22 +5,34 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="bg">
-      <section style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ display: "flex", gap: "20px" }}>
+      <section className="section-1">
+        <div className="sec-1-common">
           <span>phone</span>
           <span>email</span>
         </div>
-        <div style={{ display: "flex", gap: "20px" }}>
-          <span>a</span>
-          <span>b</span>
-          <span>c</span>
-          <span>d</span>
+        <div className="sec-1-common">
+          <span>anika</span>
+          <span>tahsin</span>
+          <span>bushra</span>
+          <span>binte</span>
         </div>
       </section>
       <section></section>
       <section></section>
     </header>
     // <div>
+    // <section style={{ display: "flex", justifyContent: "space-between" }}>
+    //     <div style={{ display: "flex", gap: "20px" }}>
+    //       <span>phone</span>
+    //       <span>email</span>
+    //     </div>
+    //     <div style={{ display: "flex", gap: "20px" }}>
+    //       <span>a</span>
+    //       <span>b</span>
+    //       <span>c</span>
+    //       <span>d</span>
+    //     </div>
+    //   </section>
     //   <img src={background} alt="" style={{ width: "100%", height: "650px" }} />
     // </div>
   );
