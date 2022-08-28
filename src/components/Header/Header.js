055a9774d86615a2d42e856101968451fdfb1a99
anikatahsin12 <bushratahsin653@gmail.com>
2022-08-28 23:57:49 +0600

    another commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,10 @@
 import React from "react";
-import background from "../../images/Background Image.png";
+import email from "../../icons/email.png";
+import phone from "../../icons/phone.png";
+import fb from "../../icons/fb.png";
+import instagram from "../../icons/instagram.png";
+import twitter from "../../icons/twitter.png";
+import youtube from "../../icons/youtube.png";
 import "./Header.css";
 
 const Header = () => {
@@ -7,18 +12,66 @@ const Header = () => {
     <header className="bg">
       <section className="section-1">
         <div className="sec-1-common">
-          <span>phone</span>
-          <span>email</span>
+          <img
+            src={email}
+            alt="info@youremail.com"
+            style={{ width: "13px", height: "10.5px", marginTop: "4px" }}
+          />{" "}
+          <span>info@youremail.com</span>
+          <img
+            src={phone}
+            alt="(480) 555-0103"
+            style={{ width: "13px", height: "10.5px", marginTop: "4px" }}
+          />{" "}
+          <span>(480) 555-0103</span>
         </div>
         <div className="sec-1-common">
-          <span>anika</span>
-          <span>tahsin</span>
-          <span>bushra</span>
-          <span>binte</span>
+          <img
+            src={fb}
+            alt="facebook.com"
+            style={{ width: "13px", height: "10.5px", marginTop: "4px" }}
+          />
+          <img
+            src={instagram}
+            alt="instagram.com"
+            style={{ width: "13px", height: "10.5px", marginTop: "4px" }}
+          />
+          <img
+            src={twitter}
+            alt="twitter.com"
+            style={{ width: "13px", height: "10.5px", marginTop: "4px" }}
+          />
+          <img
+            src={youtube}
+            alt="youtube.com"
+            style={{ width: "15px", height: "13.5px", marginTop: "4px" }}
+          />
         </div>
       </section>
-      <section></section>
-      <section></section>
+      <section className="section-2">
+        <div className="sec-2-div">
+          <span>PHONE</span>
+          <span>ABOUT</span>
+          <span>FEATURES</span>
+          <span>APPOLLY</span>
+          <span>SCREESHOT</span>
+          <span>BLOG</span>
+          <button>DOWNLOAD</button>
+        </div>
+      </section>
+      <section className="section-3">
+        <div className="sec-2-div">
+          <div className="card">
+            <title>Anika Tahsin</title>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Consequatur repudiandae vero ducimus voluptatibus deserunt nihil
+              est quibusdam autem inventore quas! Sequi cupiditate quisquam
+              vitae incidunt? Ea earum quisquam minima nihil?
+            </p>
+          </div>
+        </div>
+      </section>
     </header>
     // <div>
     // <section style={{ display: "flex", justifyContent: "space-between" }}>
