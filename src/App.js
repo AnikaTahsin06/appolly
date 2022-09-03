adf79a17e3b6b8f132cb2ab3bf125fa79a7ed17e
anikatahsin12 <bushratahsin653@gmail.com>
2022-09-03 20:40:01 +0600

    another

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import About from "./components/About/About";
 import Blog from "./components/Blog/Blog";
+import Checkout from "./components/Checkout/Checkout";
 import Features from "./components/Features/Features";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <About />
       <Features />
+      <Checkout />
       <Blog />
       <Footer />
     </div>
