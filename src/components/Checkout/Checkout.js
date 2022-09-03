d00526cccc7b15654182814f8ec85097c9d7aef6
anikatahsin12 <bushratahsin653@gmail.com>
2022-09-03 23:17:03 +0600

    another

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,38 @@
 import React from "react";
+import gravity from "../../images/Gravity.png";
+import "./Checkout.css";
 
 const Checkout = () => {
   return (
-    <div>
-      <h1>Checkout</h1>
-    </div>
+    <section>
+      <section></section>
+      <section className="checkout-section-2">
+        <div className="wholediv">
+          <div className="subdiv1">
+            <div>
+              <h1>DOWNLOAD APP NOW</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro,
+                natus laborum. Natus quibusdam neque inventore distinctio?
+                Exercitationem voluptatibus, inventore, amet vitae esse totam
+                accusamus consequuntur, porro eaque corporis saepe tempore.
+              </p>
+            </div>
+            <div>
+              <h2>Anika</h2>
+            </div>
+            <div>
+              <h2>Anika</h2>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="subdiv2">
+            <img src={gravity} alt="Gravity" />
+          </div>
+        </div>
+      </section>
+    </section>
   );
 };
 
